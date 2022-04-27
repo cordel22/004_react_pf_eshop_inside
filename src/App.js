3425aae2bb6f7be63bcd1e9067c80f04c7237e9e
cordel22 <cordelfenevall@gmail.com>
2022-04-27 17:57:03 +0200

    shit worx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,86 @@
 
 import './App.css';
 import Product from './product';
+import { useState, useEffect } from "react"
+import ProductForm from './product-form';
+/* import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"; */
+/* import { Link } from "react-router-dom"; */  //  w3s but needs lso reactrouter
+
+import ReactDOM from "react-dom/client";
+/* import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage"; */   //  w3s
+import { Link } from "react-router-dom";  // for reactrouter but used wiyh w3s too
 
 function App() {
 
-  const handler = function(name) {
+  /* const cart = []; */
+
+  const [cart, setCart] = useState([]);
+  const [data, setData] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState();
+
+  const onNewProductHandler = (product) => {
+    const newData = [...data]
+    newData.push(product)
+    console.log(newData)
+    setData(newData)
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      /* console.log("Its On") */
+    fetch('http://localhost:3001/products')
+      .then(response => 
+        {/* debugger; */
+          if (response.ok) {
+        return response.json()
+          }
+          throw new Error(`Unable to get data: ${response.statusText}`)
+        })
+        .then((json) => setData(json)) //  here we set data
+          .catch((err) => setError(err/* or */.message))
+            .finally(() => setIsPending(false))
+    }, 2000)
+     
+  }, [])
+
+/*   const handler = function(name) {
     console.log(name + ' Clicked')
+  } */
+
+  // const handler = function(product) {
+  //   /* console.log(product + ' Clicked') */
+  //   const newCart = [...cart]
+  //   newCart.push(product)
+  //   console.log(newCart)
+  //   setCart(newCart)
+  // }
+
+  const addToCartHandler = function(product) {
+
+    const newCart = [...cart]
+    newCart.push(product)
+    console.log(newCart)
+    setCart(newCart)
+  }
+
+  const removeFromCartHandler = function(product) {
+    const newCart = [...cart]
+    const productIndex = cart.findIndex(item => item.id == product.id)
+    
+    newCart.splice(productIndex, 1)
+    
+    setCart(newCart)
   }
 
 /*   const teslaProduct = {
@@ -23,7 +98,7 @@ function App() {
     price: 7
   }
  */
-  const data = [
+  /* const data = [
     {
       id: 1,
       name: "Tesla",
@@ -39,15 +114,101 @@ function App() {
       name: "Nokia",
       price: 7
     }
-  ]
+  ] */
 
   return (
-    <div className="App">
-      {data.map((item) => <Product key={item.id} product = {item} onClickHandler = {handler}/>)}
+/*     <Router> */
+<>
+{/* //  from w3s
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter> */}    
+    <div className="App">   {/* u need this line */}
+
+    <div>
+      <h1>App navigatin in App.js</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+        {/* <Link to="/cart">Cart</Link> */}
+      </nav>
+    </div>    {/* from reacrouter */}
+
+
+{/*       <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/edit-product">Edit Product</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path = "/cart">
+          <h1>Shopping Cart</h1>
+          {cart.map(item=> <div>{item.name} <button onClick = {removeFromCartHandler}>-</button></div>)}
+        </Route>
+        <Route path = "/edit-product">
+          <ProductForm onNewProduct = {onNewProductHandler} />
+        </Route>
+        <Route path = "/">
+        {<div>{cart.length}</div>}
+      {isPending && "Loading Data..."}
+      {error && <div>{error}</div>}
+        <div style = {{
+        display: "flex",
+        flexWrap: "wrap",
+        margin: "5px"
+      }}> */}
+{/*       {data.map((item) => <Product key={item.id} product = {item} onClickHandler = {addToCartHandler */}{/* handler */}{/* />)} */}
+{/*       </div>
+      
+      <ProductForm onNewProduct = {onNewProductHandler}/>
+        </Route>
+      </Switch> */}
+
+      {/* {<div>isPending && cart.length</div>} */}
+      {<div>{cart.length}</div>}          {/* u need this line */}
+      {isPending && "Loading Data..."}    {/* u need this line */}
+      {error && <div>{error}</div>}       {/* u need this line */}
+
+      <div style = {{
+        display: "flex",
+        flexWrap: "wrap",
+        margin: "5px"
+      }}>                                 {/* u need this line */}
+      {data.map((item) => <Product key={item.id} product = {item} onClickHandler = {addToCartHandler/* handler */}/>)}    {/* u need this line */}
+      </div>                              {/* u need this line */}
       {/* <Product product = {teslaProduct} onClickHandler = {handler}/>
       <Product product = {alcatelProduct} onClickHandler = {handler}/>
       <Product product = {nokiaProduct} onClickHandler = {handler}/> */}
+      <ProductForm onNewProduct = {onNewProductHandler}/>                   {/* u need this line */}
+    </div>                                                                  {/* u need this line */}
+
+    <div>
+    <h1>Shopping Cart</h1>
+          {cart.map(item=> <div>{item.name} <button onClick = {removeFromCartHandler}>-</button></div>)}
     </div>
+    </>
+/*     </Router> */
   );
 }
 
--- src/product.js
+++ src/product.js
@@ -1,14 +1,37 @@
-// function Product(productName, price/*,  onClickHandler */) {
-function Product(props) {
-  return (<div>
+import { useState } from "react"
+
+/* function Product({productName, price,  onClickHandler }) { */
+function Product({product, onClickHandler}) {
+/* function Product(props) { */
+
+const [isInCart, setIsInCart] = useState(false);
+
+  return (<div style={{
+    width: "150px",
+    border: "1px solid black",
+    display: "flex",
+    flexDirection: "column",
+    margin: "5px",
+    padding: "5px"
+  }}>
     {/* <h2>Product Name</h2> */}
     {/* <h2>{productName}</h2> */}
-    <h2>{props.product.name}</h2>
+    <h2>{product.name}</h2>
+    {/* <h2>{props.product.name}</h2> */}
     {/* <div>Price CZK</div> */}
     {/* <div>{price}</div> */}
-    <div>{props.product.price}</div>
-    {/* <button onClick = {onClickHandler}>Buy</button> */}
-    <button onClick = {() => props.onClickHandler(props.product.name)}>Buy</button>
+    <div>{product.price}</div>
+    <div><img src={product.image} height = {150} width = {150}/></div>
+
+    <div>{isInCart && "In Cart"}</div>
+
+    {/* <div>{props.product.price}</div> */}
+    {/* <button onClick = {onClickHandler(productName)}>Buy</button> */}
+    <button onClick = {() => {
+      setIsInCart(true);
+      onClickHandler(product/* .name */)
+    }}>Purchase</button>
+    {/* <button onClick = {() => props.onClickHandler(props.product.name)}>Buy</button> */}
   </div>)
 }
 
